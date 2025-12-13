537699947978c28708807463e16220b1fba5c663
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-12-13 02:12:22 +0100

    class recurssion_binary has been added

--- binary_search.py
+++ binary_search.py
@@ -27,15 +27,47 @@ class binary:
             print(f"Traget found at index : {index}")
         else:
             print("Target Not Found")
+  
+  
+class recursive_binary:
+    
+    def __init__(self,ls,target):
+        self.ls = ls
+        self.target = target
+    
+    def recurisive_binary_search(self):
+        
+        if len(self.ls) == 0:
+            return False
+        else:
+            mid = (len(self.ls))//2
+            
+            if self.ls[mid] == self.target:
+                return True
+            else:
+                if self.ls[mid] < self.target:
+                    return recursive_binary(self.ls[mid+1:],self.target).recurisive_binary_search()
+                else:
+                    return recursive_binary(self.ls[:mid],self.target).recurisive_binary_search()
+            
+    
+    def verify(self,result):
+            print("Element Found",result)
+ 
+ 
+  
         
 def main(ls,target):
     search = binary(ls,target)
     index = search.binary_search()
     search.verify(index)
+    search = recursive_binary(ls,target)
+    result = search.recurisive_binary_search()
+    search.verify(result)
     
     
 if __name__ == "__main__":
     ls = range(1,11)
-    target = 4
+    target = 2
     main(ls,target)
     
